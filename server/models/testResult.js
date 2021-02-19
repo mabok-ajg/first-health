28de547412ad309b42d2b52ecc8b086c267e6697
gon <iafgon@yahoo.com>
2021-02-19 23:00:45 +0700

    fix query test-result

--- server/models/testResult.js
+++ server/models/testResult.js
@@ -13,7 +13,7 @@ class TestResult {
     }
 
     static getTestResultByCustId(cust_id) {
-        const query = "select id, hasil, tanggal, nilai_rujukan, jenis_pemeriksaan, keterangan from test_result where id = ?"
+        const query = "select id, hasil, tanggal, nilai_rujukan, jenis_pemeriksaan, keterangan from test_result where id_customer = ?"
         return new Promise((resolve, reject) => {
             conn.query(query, cust_id, function (err, res, field) {
                 if (err) {
