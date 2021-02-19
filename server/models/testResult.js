eb281aa89e7cfbb6f5564114b12dd4b8959f03dd
gon <iafgon@yahoo.com>
2021-02-19 22:55:07 +0700

    fix typo

--- server/models/testResult.js
+++ server/models/testResult.js
@@ -13,7 +13,7 @@ class TestResult {
     }
 
     static getTestResultByCustId(cust_id) {
-        const query = "select * from test_result join customers on test_result.id_customer = customers.id where test_result.id_customer = ?"
+        const query = "select id, hasil, tanggal, nilai_rujukan, jenis_pemeriksaan, keterangan from test_result where id = ?"
         return new Promise((resolve, reject) => {
             conn.query(query, cust_id, function (err, res, field) {
                 if (err) {
