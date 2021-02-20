195d6cbed3ee4494ce988e8b8668676fd78cd296
gon <iafgon@yahoo.com>
2021-02-21 02:55:19 +0700

    add route findbyname

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,6 +3,7 @@ const CustCtrl = require('../controllers/customerController')
 
 router.post('/', CustCtrl.newCustomer)
 router.get('/', CustCtrl.getCustomers)
+router.get('/search', CustCtrl.findByName)
 router.get('/:id', CustCtrl.findCustById)
 router.get('/:id/test-results', CustCtrl.getTestResultByCustId)
 router.delete('/:id', CustCtrl.delCustomer)
